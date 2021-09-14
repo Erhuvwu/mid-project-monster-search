22c114d8355c468d68db6ee52b36ecde2ed54d2e
Erhuvwu <ayisirebless@gmail.com>
2021-09-13 23:24:37 -0500

    more styling

--- src/components/MonsterInfo.js
+++ src/components/MonsterInfo.js
@@ -4,8 +4,8 @@ function MonsterInfo(props) {
     return (
         <div className='monster-info'>
          <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
-      <h2>Name: {props.monster.name}</h2> 
-      <h2>Email: {props.monster.email}</h2>   
+      <h3>Name: {props.monster.name}</h3> 
+      <h3>Email: {props.monster.email}</h3>   
         </div>
     );
 }
